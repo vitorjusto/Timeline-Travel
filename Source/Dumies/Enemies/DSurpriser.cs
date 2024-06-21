a4ca4ee39d961af7122fb91868407fca4a9f75ec
vitorjusto <vitorhugojusto1@gmail.com>
2024-06-20 21:38:30 -0300

    put enemies level four

--- Source/Dumies/Enemies/DSurpriser.cs
+++ Source/Dumies/Enemies/DSurpriser.cs
@@ -5,11 +5,28 @@ namespace Shooter.Source.Dumies.Enemies
 {
     public class DSurpriser : IEnemyDummy
     {
+        private Vector2 _initialPosition;
+        private bool _startNextToPlayer = true;
+
+        public DSurpriser(Vector2 initialPosition)
+        {
+            _initialPosition = initialPosition;
+            _startNextToPlayer = false;
+        }
+
+
+        public DSurpriser()
+        {
+        }
+
         public Node2D GetInstance()
         {
             var scene = GD.Load<PackedScene>("res://Scenes/Enemies/Surpriser.tscn");
 
             var instance = (Surpriser)scene.Instantiate();
+
+            instance.Position = _initialPosition;
+            instance.StartNextToPlayer = _startNextToPlayer;
 
             return instance;
         }
--- Source/Models/Enemies/Surpriser.cs
+++ Source/Models/Enemies/Surpriser.cs
@@ -9,10 +9,24 @@ public partial class Surpriser : Node2D, IEnemy
 	private int _time;
     private Sprite2D _sprite;
     private Sprite2D _lightSprite;
+    public bool StartNextToPlayer = true;
 
     public override void _Ready()
     {
+        if(StartNextToPlayer)
+            SetPosition();
 
+        _lightSprite = GetNode<Sprite2D>("LevelThreeLight");
+        _lightSprite.Modulate = Color.Color8(255, 255, 255, 0);
+
+        _sprite = GetNode<Sprite2D>("Surpriser");
+        _sprite.Modulate = Color.Color8(255, 255, 255, 0);
+        _sprite.Rotation = new Random().Next(0, 90);
+    }
+
+    private void SetPosition()
+    {
+        
 		var player = GetTree().Root.GetNode<Player>("/root/Main/Player");
 
 		var x = player.Position.X  + (new Random().Next(0,3) == 1? 100: -100);
@@ -30,15 +44,8 @@ public partial class Surpriser : Node2D, IEnemy
             y = player.Position.Y - 100;
 
 		Position = new Vector2(x, y);
-
-        _lightSprite = GetNode<Sprite2D>("LevelThreeLight");
-        _lightSprite.Modulate = Color.Color8(255, 255, 255, 0);
-
-        _sprite = GetNode<Sprite2D>("Surpriser");
-        _sprite.Modulate = Color.Color8(255, 255, 255, 0);
-        _sprite.Rotation = new Random().Next(0, 90);
     }
-    
+
     public override void _Process(double delta)
     {
         _time++;
@@ -60,25 +67,25 @@ public partial class Surpriser : Node2D, IEnemy
     private void Explode()
     {
         var projectiles = GetTree().Root.GetNode<ProjectileManager>("/root/Main/ProjectileManager");
-        projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, 5, 5));
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, 5, -5));
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, -5, 5));
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, -5, -5));
+        projectiles.AddProjectile(new DNormalProjectile(Position.X + 20, Position.Y + 20, 5, 5));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X + 20, Position.Y - 20, 5, -5));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X - 20, Position.Y + 20, -5, 5));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X - 20, Position.Y - 20, -5, -5));
 
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, 5, 0));
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, 0, 5));
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, -5, 0));
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, 0, -5));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X + 10, Position.Y, 5, 0));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y + 10, 0, 5));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X - 10, Position.Y, -5, 0));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y - 10, 0, -5));
 
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, 5, 2.5f));
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, -5, 2.5f));
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, 2.5f, 5));
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, 2.5f, -5));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X + 15, Position.Y + 12.5f, 5, 2.5f));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X - 15, Position.Y + 12.5f, -5, 2.5f));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X + 12.5f, Position.Y + 15, 2.5f, 5));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X + 12.5f, Position.Y - 15, 2.5f, -5));
 
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, 5, -2.5f));
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, -5, -2.5f));
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, -2.5f, 5));
-		projectiles.AddProjectile(new DNormalProjectile(Position.X, Position.Y, -2.5f, -5));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X + 15, Position.Y - 12.5f, 5, -2.5f));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X - 15, Position.Y - 12.5f, -5, -2.5f));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X - 12.5f, Position.Y + 15, -2.5f, 5));
+		projectiles.AddProjectile(new DNormalProjectile(Position.X - 12.5f, Position.Y - 15, -2.5f, -5));
 
         var enemySpawner = GetTree().Root.GetNode<EnemySpawner>("/root/Main/EnemySpawner");
 
