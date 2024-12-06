ecd316fe795789509670afa8cd2ff5c43f629eb0
vitorjusto <vitorhugojusto1@gmail.com>
2024-12-06 18:28:04 -0300

    improve: create musics

---
+++ Source/Managers/AudioManager.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class AudioManager : Node2D
+{
+    private AudioStream audio;
+    private AudioStreamPlayer player;
+	public override void _Ready()
+	{
+                // Carregar o áudio
+        audio = ResourceLoader.Load<AudioStream>("res://Assets/Songs/OurTimeline.wav");
+
+        // Criar e configurar o AudioStreamPlayer
+        player = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+        player.Stream = audio;
+        player.Play(0);
+
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+
+    public void OnAudioFinished()
+            => player.Play(0);
+
+}
